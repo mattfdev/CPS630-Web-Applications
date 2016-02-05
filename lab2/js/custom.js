ca49f4eafc9a302579e014038acd69a2ca8157be
Matthew Francis <matthew_francis@outlook.com>
2016-02-04 22:08:30 -0500

    implemented getting location from form

--- lab2/js/custom.js
+++ lab2/js/custom.js
@@ -1,6 +1,9 @@
-
+//geolocation on button press
+var startPos;
+var city;
+var country;
+var forcast;
 document.getElementById("geolocate").onclick = function(){
-  var startPos;
   var geoSuccess = function(position) {
     startPos = position;
     console.log("Test");
@@ -11,3 +14,13 @@ document.getElementById("geolocate").onclick = function(){
   };
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 };
+
+//get location from form and submit to OpenWeather via API
+function getLocation(){
+    var queryDict = {};
+    location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
+    city = queryDict["city"];
+    country = queryDict["country"];
+    forcast = queryDict["forcast"];
+};
+
