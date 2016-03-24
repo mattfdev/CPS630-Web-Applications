5af467d8476bdbca704d66b3a18839ccf835393f
Matthew Francis <matthew_francis@outlook.com>
2016-03-24 01:14:39 -0400

    functionality complete

--- lab4/js/custom.js
+++ lab4/js/custom.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp', []); 
 app.controller('todoCtrl', function($scope) {
     $scope.todoList = [{todoText:'Clean House', description:'lost o cleaning'},{todoText:'Eat Lunch',description:'fill stomach with nutrients'}];
+    $scope.todoComplete = [{todoText:'Studty for exam',description: 'redo labs for practise'}];
     //needs to prevent duplicates
     $scope.todoAdd = function() {
         $scope.todoList.push({todoText:$scope.todoInput, description:$scope.todoDesc});
@@ -8,21 +9,29 @@ app.controller('todoCtrl', function($scope) {
         $scope.todoDesc = "";
     };
 
-    $scope.remove = function(x) {
+    function deleteEntry(x) {
         var oldList = $scope.todoList;
         $scope.todoList = [];
         var i = oldList.indexOf(x);
-        var removed = oldList.splice(i,1);
+        oldList.splice(i,1);
         angular.forEach(oldList, function(a) {
             $scope.todoList.push(a);
         }); 
     };
-    
+    $scope.remove = deleteEntry;
     $scope.finished = function(x){
-        alert('archived'+x.description);
+        $scope.todoComplete.push({todoText:x.todoText,description:x.description});
+        deleteEntry(x);
     };
     
     $scope.redo = function (x) {
-        
+        $scope.todoList.push({todoText:x.todoText,description:x.description});
+        var oldList = $scope.todoComplete;
+        $scope.todoComplete = [];
+        var i = oldList.indexOf(x);
+        oldList.splice(i,1);
+        angular.forEach(oldList, function(a) {
+            $scope.todoComplete.push(a);
+        }); 
     };
 });
