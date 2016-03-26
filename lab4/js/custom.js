40ddc8617874630d167313678241ad97845e7f96
Matthew Francis <matthew_francis@outlook.com>
2016-03-26 13:16:44 -0400

    completed lab 4 external functionality

--- lab4/js/custom.js
+++ lab4/js/custom.js
@@ -1,10 +1,17 @@
+
+
+
 var app = angular.module('myApp', []); 
 app.controller('todoCtrl', function($scope) {
-    $scope.todoList = [{todoText:'Clean House', description:'lost o cleaning'},{todoText:'Eat Lunch',description:'fill stomach with nutrients'}];
-    $scope.todoComplete = [{todoText:'Studty for exam',description: 'redo labs for practise'}];
+    var jsonData;
+    //unnecessary for final
+    //$scope.todoList = [{task:'Clean House', complete:false, description:'lost of cleaning'},{task:'Eat Lunch', complete:false, description:'fill stomach with nutrients'}];
+    //$scope.todoComplete = [{task:'Study for exam', complete:true, description: 'redo labs for practise'}];
     //needs to prevent duplicates
+    $scope.todoComplete = [];
+    $scope.todoList = [];
     $scope.todoAdd = function() {
-        $scope.todoList.push({todoText:$scope.todoInput, description:$scope.todoDesc});
+        $scope.todoList.push({task:$scope.todoInput, complete:false, description:$scope.todoDesc});
         $scope.todoInput = "";
         $scope.todoDesc = "";
     };
@@ -19,13 +26,38 @@ app.controller('todoCtrl', function($scope) {
         }); 
     };
     $scope.remove = deleteEntry;
-    $scope.finished = function(x){
-        $scope.todoComplete.push({todoText:x.todoText,description:x.description});
+ 
+    function finishEntry(x){
+        $scope.todoComplete.push({task:x.task, complete:true,description:x.description});
         deleteEntry(x);
     };
+    $scope.finished = finishEntry;
+    function jsonEntry(item){
+        $scope.todoList.push({task:item.task, complete:item.complete, description:item.description});
+    }
+    $scope.handleFiles = function(event) {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            // The file's text will be printed here
+            jsonData = JSON.parse(event.target.result);
+            var todoItems = jsonData.todo;
+            angular.forEach(todoItems,function(item){
+                if(item.complete){
+                    finishEntry(item);
+                }
+                else{
+                    jsonEntry(item);
+                }
+                
+            });
+            $scope.$apply();
+        };
+        reader.readAsText(file);
+    }
     
     $scope.redo = function (x) {
-        $scope.todoList.push({todoText:x.todoText,description:x.description});
+        $scope.todoList.push({task:x.task, complete:false, description:x.description});
         var oldList = $scope.todoComplete;
         $scope.todoComplete = [];
         var i = oldList.indexOf(x);
@@ -35,3 +67,4 @@ app.controller('todoCtrl', function($scope) {
         }); 
     };
 });
+
