3fc78c38f54bee7dea74d0da94e95c6a732fa3f6
Matthew Francis <matthew_francis@outlook.com>
2016-04-06 20:14:16 -0400

    lab 5 functionality complete

--- lab5/startapp.js
+++ lab5/startapp.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
+var fs = require("fs");
 
+function readJsonFileSync(filepath, encoding){
+
+    if (typeof (encoding) === 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
+
+function getConfig(file){
+    var filepath = __dirname + '/' + file;
+    return readJsonFileSync(filepath);
+}
+
+var articles = getConfig('articles.json');
+var about = getConfig('about.json');
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.render('body',{locals:{active: 1, username: "something"}});
+    res.render('body',articles);
 });
 
 app.get('/about', function(req, res) {
-  res.send('about',{locals:{active: 2, username: "something"}});
+  res.render('about',about);
 });
 
 app.listen(8080, function () {
